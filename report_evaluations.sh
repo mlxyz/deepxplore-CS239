a6afacb56d0b09543f356c2d5c3b7cf28deec9da
jteoh <jiateoh@gmail.com>
2019-03-07 18:42:20 -0800

    Add evaluation script for CS 239 report results

---
+++ report_evaluations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# for each iteration, prints DX-NC and True-NC (name??). Note that True-NC calculates individual neuron coverage based on the unscaled activation output.
+mkdir -p official_report_results
+
+# MNIST
+(cd MNIST && python gen_diff.py light 1 0.1 10 500 20 0) > official_report_results/mnist_500_original_light.log &
+(cd MNIST && python gen_diff.py occl 1 0.1 10 500 20 0) > official_report_results/mnist_500_original_occl.log &
+(cd MNIST && python gen_diff.py blackout 1 0.1 10 500 20 0) > official_report_results/mnist_500_original_blackout.log &
+
+# Driving
+(cd Driving && python gen_diff.py light 1 0.1 10 500 20 0) > official_report_results/driving_500_original_light.log &
+(cd Driving && python gen_diff.py occl 1 0.1 10 500 20 0) > official_report_results/driving_500_original_occl.log &
+(cd Driving && python gen_diff.py blackout 1 0.1 10 500 20 0) > official_report_results/driving_500_original_blackout.log &
+
+# PDF
+(cd PDF && python gen_diff.py 2 0.1 0.1 500 20 0) > official_report_results/pdf_500_original.log
