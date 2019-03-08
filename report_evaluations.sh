d30792e2a2e1a8024cd9b6b7f651d17b6f6378dd
jteoh <jiateoh@gmail.com>
2019-03-07 20:15:04 -0800

    Fix to report_evaluations.sh script so that stdin is properly piped to file (unknown initial problem, this is just a workaround)

--- report_evaluations.sh
+++ report_evaluations.sh
@@ -3,14 +3,15 @@
 mkdir -p official_report_results
 
 # MNIST
-(cd MNIST && python gen_diff.py light 1 0.1 10 500 20 0) > official_report_results/mnist_500_original_light.log &
-(cd MNIST && python gen_diff.py occl 1 0.1 10 500 20 0) > official_report_results/mnist_500_original_occl.log &
-(cd MNIST && python gen_diff.py blackout 1 0.1 10 500 20 0) > official_report_results/mnist_500_original_blackout.log &
 
-# Driving
-(cd Driving && python gen_diff.py light 1 0.1 10 500 20 0) > official_report_results/driving_500_original_light.log &
-(cd Driving && python gen_diff.py occl 1 0.1 10 500 20 0) > official_report_results/driving_500_original_occl.log &
-(cd Driving && python gen_diff.py blackout 1 0.1 10 500 20 0) > official_report_results/driving_500_original_blackout.log &
+(cd MNIST && python gen_diff.py light 1 0.1 10 500 20 0) | tee official_report_results/mnist_500_original_light.log > /dev/null &
+(cd MNIST && python gen_diff.py occl 1 0.1 10 500 20 0) | tee official_report_results/mnist_500_original_occl.log > /dev/null &
+(cd MNIST && python gen_diff.py blackout 1 0.1 10 500 20 0) | tee official_report_results/mnist_500_original_blackout.log > /dev/null &
 
-# PDF
-(cd PDF && python gen_diff.py 2 0.1 0.1 500 20 0) > official_report_results/pdf_500_original.log &
+# # Driving
+(cd Driving && python gen_diff.py light 1 0.1 10 500 20 0) | tee official_report_results/driving_500_original_light.log > /dev/null &
+(cd Driving && python gen_diff.py occl 1 0.1 10 500 20 0) | tee official_report_results/driving_500_original_occl.log > /dev/null &
+(cd Driving && python gen_diff.py blackout 1 0.1 10 500 20 0) | tee official_report_results/driving_500_original_blackout.log > /dev/null &
+
+# # PDF
+(cd PDF && python gen_diff.py 2 0.1 0.1 500 20 0) | tee official_report_results/pdf_500_original.log > /dev/null &
